e73d26bed7450dbd7a421628e51d5c09114745b6
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-05-10 17:12:15 +0530

    [Rahul]Generic class custom LinkedList problem

---
+++ DataStructurePractices/GenericLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePractices
+{
+    public class GenericLinkedList<T>
+    {
+        public T var1 , var2;
+        public GenericLinkedList(T var1, T var2)
+        {
+           this.var1 = var1;
+           this.var2 = var2;
+           Console.WriteLine(var1+" "+var2);
+        }
+        public void CustomLinkedList(T var1 , T var2)
+        {
+            Console.WriteLine(var1+" "+var2);
+            LinkedList<int> linkedlist = new LinkedList<int>();
+            linkedlist.AddFirst(121);
+            linkedlist.AddFirst(131);
+            linkedlist.AddLast(141);
+            linkedlist.AddLast(151);
+            foreach(var item in linkedlist)
+            {
+                Console.WriteLine("LinkedList :"+item);
+            }
+        }
+    }
+}
--- DataStructurePractices/Program.cs
+++ DataStructurePractices/Program.cs
@@ -11,6 +11,18 @@ namespace DataStructurePractices
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Data Structure");
+            Console.WriteLine("Choose the following");
+            Console.WriteLine("1.Generic LinkedList");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    GenericLinkedList<int> genericLinkedList = new GenericLinkedList<int>(45,55);
+                    GenericLinkedList<string> genericLinkedList1 = new GenericLinkedList<string>("Raj", "Neha");
+                    GenericLinkedList<double> generic = new GenericLinkedList<double>(45.5, 12.8);
+                    genericLinkedList.CustomLinkedList(88,65);
+                    break;
+            }
             Console.ReadLine();
         }
     }
